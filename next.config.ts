6f21e6c2d2661faba78a5f9845aed671cc240145
kshare10 <kyleshare1@gmail.com>
2026-08-23 15:59:11 -0700

    Add quality 92 to images config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    qualities: [75, 85],
+    qualities: [75, 85, 92],
   },
 };
 
